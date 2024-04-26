9795a8cdca142e694a9778111b5abac47d0933ae
andrewraylee <38772559+andrewraylee@users.noreply.github.com>
2024-04-26 13:37:47 -0500

    beautiful re-write of the display rendering

--- Chip8Display.cpp
+++ Chip8Display.cpp
@@ -6,8 +6,11 @@
 SDL_Rect rectArray[64][32];
 SDL_Window *window;
 SDL_Renderer *renderer;
+SDL_Surface *surface;
+SDL_Texture *texture;
 
 BYTE (*screenData)[64][32];
+Uint32 pixels[640 * 320];
 
 void Chip8Display::Initialize() {
 
@@ -58,19 +61,24 @@ void Chip8Display::InitRectArray(SDL_Rect pRect[64][32]) {
 }
 
 void Chip8Display::UpdateScreen(SDL_Renderer *pRenderer, Chip8Core& chip8Core) {
+    memset(pixels, 0, sizeof(pixels));
     screenData = chip8Core.GetScreenData();
     for (int i = 0; i < 64; i++) {
         for (int j = 0; j < 32; j++) {
             if ((*screenData)[i][j]) {
-                SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
-                SDL_RenderFillRect(pRenderer, &rectArray[i][j]);
-            }
-            else {
-                SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
-                SDL_RenderFillRect(pRenderer, &rectArray[i][j]);
+                for (int x = 0; x < 10; x++) {
+                    for (int y = 0; y < 10; y++) {
+                        pixels[(j * 10 + y) * 640 + (i * 10 + x)] = 0xFFFFFFFF;
+                    }
+                }
             }
         }
     }
+    surface = SDL_CreateRGBSurfaceFrom(pixels, 640, 320, 32, 640 * 4, 0, 0, 0, 0);
+    texture = SDL_CreateTextureFromSurface(pRenderer, surface);
+    SDL_RenderCopy(pRenderer, texture, nullptr, nullptr);
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(surface);
 }
 
 void Chip8Display::RenderScreen(SDL_Renderer *pRenderer) {
